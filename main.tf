bc9e9b655f18639908b9be20f77a2af4a485cff1
pkranjan <pkranjan@hotmail.com>
2020-02-23 14:51:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+data "null_data_source" "environment" {
+    inputs = {
+        environment = "${lower(replace(var.tag_environment,"/[^a-zA-Z0-9]/",""))}"
+    }
+}
+
+output "environmant" {
+    value = "${data.null_data_source.environment.outputs["environment"]}"
+}
+
+data "aws_iam_policy_document" "sfn_assume_role_policy_document" {
+
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "states.us-east-1.amazonaws.com",
+        "events.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "iam_for_sfn" {
+  name = "tf-${terraform.workspace}-iam_for_sfn"
+  assume_role_policy = data.aws_iam_policy_document.sfn_assume_role_policy_document.json
+}
+
+resource "aws_sfn_state_machine" "sfn_state_machine" {
+  name     = "my-state-machine"
+  role_arn = aws_iam_role.iam_for_sfn.arn
+
+  definition = <<EOF
+{
+  "Comment": "A Hello World example of the Amazon States Language using an AWS Lambda Function...",
+  "StartAt": "HelloWorld",
+  "States": {
+    "HelloWorld": {
+      "Type": "Task",
+      "Resource": "arn:aws:lambda:us-east-1:333463754612:function:run_task",
+      "End": true
+    }
+  }
+}
+EOF
+}
