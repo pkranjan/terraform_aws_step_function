556e61aec1fea7fa5afc8a59e3f7f88e12fb3875
pkranjan <pkranjan@hotmail.com>
2020-02-23 15:09:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ data "aws_iam_policy_document" "sfn_assume_role_policy_document_module" {
 }
 
 resource "aws_iam_role" "iam_for_sfn_module" {
-  name = "tf-${terraform.workspace}-iam_for_sfn"
+  name = "tf-${terraform.workspace}-iam_for_sfn_module"
   assume_role_policy = data.aws_iam_policy_document.sfn_assume_role_policy_document_module.json
 }
 
